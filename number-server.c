50cf5e688a99dd5bf32c3e121b1bca0e6ac97602
Joe Politz <joe.politz@gmail.com>
2024-11-04 20:44:44 -0800

    Revert "lecture-code"
    This reverts commit cebcc22e7b33b2461c09f1b6ec874ebc37549e6f.

--- number-server.c
+++ number-server.c
@@ -1,10 +1,9 @@
 #include "http-server.h"
 #include <string.h>
 
-int num = 0; // the state of the server (the hello world version of chats list)
+int num = 0;
 
 char const HTTP_404_NOT_FOUND[] = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
-char const HTTP_200_OK[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
 
 void handle_404(int client_sock, char *path)  {
     printf("SERVER LOG: Got request for unrecognized path \"%s\"\n", path);
@@ -14,24 +13,8 @@ void handle_404(int client_sock, char *path)  {
     // snprintf includes a null-terminator
 
     // TODO: send response back to client?
-    write(client_sock, HTTP_404_NOT_FOUND, strlen(HTTP_404_NOT_FOUND));
-    write(client_sock, response_buff, strlen(response_buff));
 }
 
-void handle_root(int client_sock) {
-  char message[BUFFER_SIZE];
-  snprintf(message, BUFFER_SIZE, "Current number: %d\n", num);
-  write(client_sock, HTTP_200_OK, strlen(HTTP_200_OK));  
-  write(client_sock, message, strlen(message));
-}
-
-void handle_increment(int client_sock) {
-  char message[BUFFER_SIZE];
-  num += 1;
-  snprintf(message, BUFFER_SIZE, "Incremented to: %d\n", num);
-  write(client_sock, HTTP_200_OK, strlen(HTTP_200_OK));  
-  write(client_sock, message, strlen(message));
-}
 
 void handle_response(char *request, int client_sock) {
     char path[256];
@@ -44,24 +27,7 @@ void handle_response(char *request, int client_sock) {
         return;
     }
 
-    /* "/" – shows “Current number: ____”
-        "/increment" – adds 1 to number and shows “Incremented to: _____” */
-    // How to write the if statements to detect which path we have?
-    if(strcmp(path, "/") == 0) {
-      handle_root(client_sock);
-      return;
-    }
-    else if(strcmp(path, "/increment") == 0) {
-      handle_increment(client_sock);
-      return;
-    }
-    else {
-      handle_404(client_sock, path);
-    }
-
-    // strstr if there might be shared prefixes, like looking for "/post" in the PA
-    // save strstr for later
-
+    handle_404(client_sock, path);
 }
 
 int main(int argc, char *argv[]) {
